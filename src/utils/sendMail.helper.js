3bb247d4a537b8a334ea1ceac824ecf2a0369b0d
ronak <ronakdobariya74340@gmail.com>
2025-10-18 23:16:45 +0530

    TC

--- src/utils/sendMail.helper.js
+++ src/utils/sendMail.helper.js
@@ -4,18 +4,39 @@ dotenv.config();
 const { successAction, failAction } = require("./response");
 const Message = require("./messages");
 
+// const transport = nodemailer.createTransport({
+//   host: process.env.HOST,
+//   port: process.env.MAIL_PORT,
+//   secure: false,
+//   auth: {
+//     user: process.env.MAIL_USERNAME,
+//     pass: process.env.MAIL_PASSWORD,
+//   },
+// });
+
+// export const sendMail = async (mailOptions) => {
+//   console.log("sendMail called------------>", process.env.HOST);
+//   await transport.sendMail(mailOptions, async (error, info) => {
+//     if (error) {
+//       console.log("sendMail error------------>", error);
+//       return failAction(Message.unexpectedDataError);
+//     } else {
+//       console.log("sendMail info response------------>", info.response);
+//       return successAction({}, `Email sent --------> ${info.response}`);
+//     }
+//   });
+// };
+
 const transport = nodemailer.createTransport({
-  host: process.env.HOST,
-  port: process.env.MAIL_PORT,
-  secure: false,
+  service: "gmail",
   auth: {
-    user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD,
+    user: process.env.GMAIL_USER, // your Gmail address
+    pass: process.env.GMAIL_APP_PASSWORD, // your App Password (not your normal password)
   },
 });
 
 export const sendMail = async (mailOptions) => {
-  console.log("sendMail called------------>", process.env.HOST);
+  // console.log("sendMail called------------>", process.env.HOST);
   await transport.sendMail(mailOptions, async (error, info) => {
     if (error) {
       console.log("sendMail error------------>", error);
